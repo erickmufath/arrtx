53b4f6690192a4f0b601b7acb1d9187ff133b0f1
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-12 22:49:02 +0700

    Create basestrap.sh

---
+++ basestrap.sh
@@ -0,0 +1,33 @@
+pacman -Sy sed --noconfirm --needed
+#Add parallel downloading
+sed -i 's/^#Para/Para/' /etc/pacman.conf
+#sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 2/' /etc/pacman.conf
+#Enable multilib
+sed -i "/\[lib32\]/,/Include/"'s/^#//' /etc/pacman.conf
+sed -i "/\[lib32-gremlins\]/,/Include/"'s/^#//' /etc/pacman.conf
+#basestrap
+basestrap /mnt base base-devel linux linux-firmware runit elogind-runit nano
+fstabgen -U /mnt >> /mnt/etc/fstab
+echo "======================================================"
+echo "=] 1. BIOS/Legacy"
+echo "=] 2. UEFI [Beta Test]"
+echo "=] 3. HYBRID [Direkomendasikan Untuk Media External/Beta test]"
+echo "======================================================="
+read -p ">> Pilih Jenis Boot (1/2/3) : " boot
+read -p "->] Input Nama Komputer Arch Linux (Contoh : pc atau acer) = " hstname
+read -p "->] Input Username = " hstname
+read -p "->] Input Jumlah ZRAM (Contoh 8192) terhitung satuan Mega Byte = " zramd
+
+echo ""
+echo ""
+lsblk
+echo ""
+echo ""
+read -p "->] Input Drive untuk install Bootloader (Contoh : sda, sdb, atau nvmen1) = " drive
+clear
+echo "zramd=$zramd" >> arrtx/install.conf
+echo "boot=$boot" >> arrtx/install.conf
+echo "drive=$drive" >> arrtx/install.conf
+echo "usrname=$usrname" >> arrtx/install.conf
+cp -rf arrtx /mnt
+artix-chroot /mnt

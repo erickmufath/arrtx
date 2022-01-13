d64bd5d8d6137d5657184ffcf53da82539f2c62f
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-14 05:19:38 +0700

    Update basestrap.sh

--- basestrap.sh
+++ basestrap.sh
@@ -39,3 +39,4 @@ echo "zramd=$zramd" >> arrtx/install.conf
 echo "boot=$boot" >> arrtx/install.conf
 echo "hstname=$hstname" >> arrtx/install.conf
 echo "drive=$drive" >> arrtx/install.conf
+cp -rf arrtx /mnt

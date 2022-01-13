00ee351a93b872b7df1a1f4805986fe9bd767292
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-13 20:26:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ arrtx/basestrap.sh
 artix-chroot /mnt arrtx/pacman.sh
 artix-chroot /mnt arrtx/multilib.sh
 source arrtx/install.conf
-artix-chroot /mnt /usr/bin/runuser -u ${usrname} -- arrtx/yayopenbox.sh
+artix-chroot /mnt /usr/bin/runuser -u ${usrname} -- arrtx/aftereboot.sh

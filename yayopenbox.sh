6996a99f37d0be44745ce2004b05d62a37e91be3
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-13 16:28:57 +0700

    Update yayopenbox.sh

--- yayopenbox.sh
+++ yayopenbox.sh
@@ -1,18 +1,18 @@
 sudo ln -s /etc/runit/sv/NetworkManager /run/runit/service
 source arrtx/install.conf
-git clone https://github.com/addy-dclxvi/openbox-theme-collections /.themes
-rm -rf /.config/tint2
-git clone https://github.com/addy-dclxvi/tint2-theme-collections /.config/tint2 --depth 1
-cp ~/.config/tint2/minima/minima.tint2rc /.config/tint2/tint2rc
-mkdir /.config/openbox
-cp ~/arrtx/rc.xml /.config/openbox
-cp ~/arrtx/autostart /.config/openbox
+git clone https://github.com/addy-dclxvi/openbox-theme-collections ~/.themes
+rm -rf ~/.config/tint2
+git clone https://github.com/addy-dclxvi/tint2-theme-collections ~/.config/tint2 --depth 1
+cp ~/.config/tint2/minima/minima.tint2rc ~/.config/tint2/tint2rc
+mkdir ~/.config/openbox
+cp ~/arrtx/rc.xml ~/.config/openbox
+cp ~/arrtx/autostart ~/.config/openbox
 git clone https://aur.archlinux.org/yay.git 
 cd yay
 makepkg -si --noconfirm
 cd 
 yay -Syyu
-yay -Syu --needed - < arrtx/aur.txt
+yay -Sy --needed - < arrtx/aur.txt
 sudo ln -s /etc/runit/sv/bluetoothd /run/runit/service
 sudo ln -s /etc/runit/sv/cupsd /run/runit/service
 sudo ln -s /etc/runit/sv/ntpd /run/runit/service

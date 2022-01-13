853bc34874ac1cdf9de749809d5a35582f8b2447
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-13 16:22:29 +0700

    Update and rename after-reboot.sh to multilib.sh

--- multilib.sh
+++ multilib.sh
@@ -1,15 +1,9 @@
-ln -s /etc/runit/sv/NetworkManager /run/runit/service
-nmtui
 source arrtx/install.conf
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy artix-archlinux-support pacman --noconfirm --needed
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy artix-archlinux-support pacman --noconfirm --needed
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy artix-archlinux-support pacman --noconfirm --needed
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy artix-archlinux-support pacman --noconfirm --needed
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy artix-archlinux-support pacman --noconfirm --needed
-pacman-key --init
-pacman-key --import archlinux artix
-pacman-key --populate archlinux artix
-pacman -Syyu
 #sed -i 's/^Server/#Server/' /etc/pacman.conf
 #echo "Server = https://artix.wheaton.edu/repos/$repo/os/$arch" >> /etc/pacman.d/mirrorlist
 #echo "Server = https://mirror.clarkson.edu/artix-linux/repos/$repo/os/$arch" >> /etc/pacman.d/mirrorlist
@@ -20,6 +14,10 @@ echo "[extra]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
 echo "[community]" >> /etc/pacman.conf
 echo "Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
+rm -r /etc/pacman.d/gnupg
+pacman-key --init
+pacman-key --import archlinux artix
+pacman-key --populate archlinux artix
 pacman -Syyu
 sudo cp -rf arrtx/xorg.conf /etc/X11/
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy rsync ntp git ttf-caladea nomacs scrcpy ttf-anonymous-pro ttf-cascadia-code ttf-cormorant ttf-eurof ttf-ibm-plex ttf-jetbrains-mono ttf-joypixels ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-proggy-clean firefox adapta-gtk-theme materia-gtk-theme tint2 nitrogen obconf --noconfirm --needed
@@ -33,7 +31,3 @@ sudo cp -rf arrtx/xorg.conf /etc/X11/
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy rsync ntp git ttf-caladea nomacs scrcpy ttf-anonymous-pro ttf-cascadia-code ttf-cormorant ttf-eurof ttf-ibm-plex ttf-jetbrains-mono ttf-joypixels ttf-linux-libertine ttf-linux-libertine-g ttf-monofur ttf-proggy-clean firefox adapta-gtk-theme materia-gtk-theme tint2 nitrogen obconf --noconfirm --needed
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-lirc pulseaudio-zeroconf lsp-plugins pulsemixer --noconfirm --needed
-ln -s /etc/runit/sv/bluetoothd /run/runit/service
-ln -s /etc/runit/sv/cupsd /run/runit/service
-ln -s /etc/runit/sv/ntpd /run/runit/service
-ln -s /etc/runit/sv/sddm /run/runit/service

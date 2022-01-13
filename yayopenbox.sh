7674281c451b4666b1bcc787f877988d78922f01
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-13 08:40:53 +0700

    Create yayopenbox.sh

---
+++ yayopenbox.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/addy-dclxvi/openbox-theme-collections ~/.themes
+rm -rf ~/.config/tint2
+git clone https://github.com/addy-dclxvi/tint2-theme-collections ~/.config/tint2 --depth 1
+cp ~/.config/tint2/minima/minima.tint2rc ~/.config/tint2/tint2rc
+mkdir ~/.config/openbox
+cp ~/arrtx/rc.xml ~/.config/openbox
+cp ~/arrtx/autostart ~/.config/openbox
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+yay -Syyu
+yay -Sy --noconfirm --needed - < arrtx/aur.txt

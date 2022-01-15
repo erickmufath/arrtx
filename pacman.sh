c6adc39b3c71940dad539a1138d1dc120e149e6c
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-15 14:56:35 +0700

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -44,11 +44,7 @@ cp -rf arrtx /home/${usrname}/
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/user-pkgs.txt
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/user-pkgs.txt
 /usr/bin/runuser -u ${usrname} -- sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/user-pkgs.txt
-read -p "->] Install Lutris For Gaming (y/n) : " gam
-case $gam in
-y|Y|yes|Yes|YES)
-sudo pacman -Sy gamemode dxvk winetricks lutris giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm --needed
-esac
+sh arrtx/game.sh
 # Graphics Drivers find and install
 gpu_type=$(lspci)
 if lspci | grep -E "NVIDIA|GeForce"  <<< ${gpu_type};  then
@@ -57,29 +53,44 @@ echo    ""
 echo    "           INSTALLING NVIDIA GRAPHICS DRIVER"
 echo    ""
 echo    "--------------------------------------------------------"
-    sudo pacman -Sy nvidia nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm --needed
-	nvidia-xconfig
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/nvidia-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/nvidia-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/nvidia-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/nvidia-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/nvidia-pkgs.txt
 elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
     echo    "--------------------------------------------------------"
 echo    ""
 echo    "           INSTALLING AMD GRAPHICS DRIVER"
 echo    ""
 echo    "--------------------------------------------------------"
-    sudo pacman -Sy xf86-video-amdgpu lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader --noconfirm --needed
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/amd-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/amd-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/amd-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/amd-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/amd-pkgs.txt
 elif lspci grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
     echo    "--------------------------------------------------------"
 echo    ""
 echo    "           INSTALLING INTEL GRAPHICS DRIVER"
 echo    ""
 echo    "--------------------------------------------------------"
-    sudo pacman -Sy libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader --needed --noconfirm
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
 elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; <<< ${gpu_type}; then
     echo    "--------------------------------------------------------"
 echo    ""
 echo    "           INSTALLING INTEL GRAPHICS DRIVER"
 echo    ""
 echo    "--------------------------------------------------------"
-    sudo pacman -Sy libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader --needed --noconfirm
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
+    sudo pacman -Sy --noconfirm --needed - < arrtx/pkgs/intel-pkgs.txt
 fi
 case $boot in
 1)

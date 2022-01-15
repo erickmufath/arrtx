d2fb714fc0e7533a83b1701254da9e45302660b6
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-15 12:03:13 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 arrtx/basestrap.sh
 artix-chroot /mnt arrtx/pacman.sh
 artix-chroot /mnt arrtx/multilib.sh
-source /mnt/arrtx/install.conf
-artix-chroot /mnt /usr/bin/runuser -u ${usrname} -- /home/${usrname}/arrtx/aftereboot.sh

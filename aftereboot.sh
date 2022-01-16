1a5f8b730c510d60fca5740f3e8efe87ce1d4043
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-16 23:11:41 +0700

    Update aftereboot.sh

--- aftereboot.sh
+++ aftereboot.sh
@@ -5,11 +5,15 @@ sleep 5
 nmtui
 /usr/bin/runuser -u ${usrname} -- arrtx/yay.sh
 sed -i 's/^# MAX_SIZE=8192/MAX_SIZE=$zramd/' /etc/default/zramd
-echo -e "\nSetup SDDM Theme"
+#detect and set sddm nordic theme
+sddm_ava=$(pacman -Qqe)
+if grep -E "sddm-nordic-theme-git" <<< ${sddm_ava}; then
+  echo -e "\nSetup SDDM Theme"
 cat <<EOF > /etc/sddm.conf
 [Theme]
 Current=Nordic
-EOF
+EOF  
+fi
 sleep 5
 ln -s /etc/runit/sv/bluetoothd /run/runit/service
 ln -s /etc/runit/sv/cupsd /run/runit/service

3e99f5498b5a2a4c58923c9366fb0e243f278542
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-16 22:50:27 +0700

    Create aftereboot.sh

---
+++ aftereboot.sh
@@ -0,0 +1,17 @@
+source /arrtx/install.conf
+read -p "Make sure you are in root (press enter to continue or ctrl+c to abort) : "
+ln -s /etc/runit/sv/NetworkManager /run/runit/service
+sleep 5
+nmtui
+/usr/bin/runuser -u ${usrname} -- arrtx/yay.sh
+sed -i 's/^# MAX_SIZE=8192/MAX_SIZE=$zramd/' /etc/default/zramd
+echo -e "\nSetup SDDM Theme"
+cat <<EOF > /etc/sddm.conf
+[Theme]
+Current=Nordic
+EOF
+sleep 5
+ln -s /etc/runit/sv/bluetoothd /run/runit/service
+ln -s /etc/runit/sv/cupsd /run/runit/service
+ln -s /etc/runit/sv/ntpd /run/runit/service
+ln -s /etc/runit/sv/sddm /run/runit/service

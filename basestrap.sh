7d4712ebf49803cae9451677ba7b226062aa04e8
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-16 14:28:00 +0700

    Update basestrap.sh

--- basestrap.sh
+++ basestrap.sh
@@ -1,3 +1,18 @@
+echo "======================================================"
+echo "=] 1. BIOS/Legacy"
+echo "=] 2. UEFI [Beta Test]"
+echo "=] 3. HYBRID [Direkomendasikan Untuk Media External/Beta test]"
+echo "======================================================="
+read -p ">> Pilih Jenis Boot (1/2/3) : " boot
+read -p "->] Input Nama Komputer Arch Linux (Contoh : pc atau acer) = " hstname
+read -p "->] Input Jumlah ZRAM (Contoh 8192) terhitung satuan Mega Byte = " zramd
+
+echo ""
+echo ""
+lsblk
+echo ""
+echo ""
+read -p "->] Input Drive untuk install Bootloader (Contoh : sda, sdb, atau nvmen1) = " drive
 pacman -Sy sed artix-archlinux-support pacman --noconfirm --needed
 #Add parallel downloading
 sed -i 's/^#Para/Para/' /etc/pacman.conf
@@ -19,21 +34,6 @@ basestrap /mnt --needed - < arrtx/pkgs/base-pkgs.txt
 basestrap /mnt --needed - < arrtx/pkgs/base-pkgs.txt
 basestrap /mnt --needed - < arrtx/pkgs/base-pkgs.txt
 fstabgen -U /mnt >> /mnt/etc/fstab
-echo "======================================================"
-echo "=] 1. BIOS/Legacy"
-echo "=] 2. UEFI [Beta Test]"
-echo "=] 3. HYBRID [Direkomendasikan Untuk Media External/Beta test]"
-echo "======================================================="
-read -p ">> Pilih Jenis Boot (1/2/3) : " boot
-read -p "->] Input Nama Komputer Arch Linux (Contoh : pc atau acer) = " hstname
-read -p "->] Input Jumlah ZRAM (Contoh 8192) terhitung satuan Mega Byte = " zramd
-
-echo ""
-echo ""
-lsblk
-echo ""
-echo ""
-read -p "->] Input Drive untuk install Bootloader (Contoh : sda, sdb, atau nvmen1) = " drive
 clear
 echo "zramd=$zramd" >> arrtx/install.conf
 echo "boot=$boot" >> arrtx/install.conf

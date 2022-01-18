1450db016ee3f59e78b7000a473bf5bdd83779c4
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-18 16:36:17 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,3 @@ arrtx/basestrap.sh
 artix-chroot /mnt arrtx/user.sh
 artix-chroot /mnt arrtx/multilib.sh
 artix-chroot /mnt arrtx/pacman.sh
-source /mnt/arrtx/install.conf
-artix-chroot /mnt /usr/bin/runuser -u ${usrname} -- arrtx/yay.sh

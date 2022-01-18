0bfc66b52763c83ebf6b0cae1d0ffef900e8d20c
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-18 14:58:06 +0700

    Update aftereboot.sh

--- aftereboot.sh
+++ aftereboot.sh
@@ -9,11 +9,8 @@ ln -s /etc/runit/sv/bluetoothd /run/runit/service
 ln -s /etc/runit/sv/cupsd /run/runit/service
 ln -s /etc/runit/sv/ntpd /run/runit/service
 ln -s /etc/runit/sv/sddm /run/runit/service
-#detect and set sddm nordic theme
-sddm_ava=$(pacman -Qqe)
-if grep -E "sddm-nordic-theme-git" <<< ${sddm_ava}; then
+#set sddm nordic theme
 cat <<EOF > /etc/sddm.conf
 [Theme]
 Current=Nordic
 EOF
-fi
